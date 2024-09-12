0906016e0ca6b740b015785620d6f905c6a80af2
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-09-12 09:00:50 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Prompt } from "next/font/google";
 import "./globals.css";
 
 const prompt = Prompt({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "800"],
+  subsets: [ "latin" ],
+  weight: [ "300", "400", "500", "600", "700", "800" ],
 });
 
 export const metadata: Metadata = {
@@ -16,9 +16,10 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>)
+{
   return (
-    <html lang="en">
+    <html lang="en" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
       <body className={`${prompt.className} min-h-[calc(100dvh)] bg-white`}>
         {children}
       </body>
